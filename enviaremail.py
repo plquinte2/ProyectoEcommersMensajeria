126dacd49c579d5c8b2fb284ebd894c64c5674db
Luis Fernando Quintero Niño <112994319+plquinte2@users.noreply.github.com>
2022-10-08 08:24:23 -0500

    proyecto finalizadp

--- enviaremail.py
+++ enviaremail.py
@@ -1,14 +1,33 @@
 from email.message import EmailMessage
 import smtplib
+
 def enviar_email(email_destino,codigo):
     remitente = "quinterolf@uninorte.edu.co"
     destinatario = email_destino
-    mensaje = "Ingrese el siguiente codigo: "+codigo+" para activar su cuenta"
+    mensaje = "Codigo de Activacion: "+ codigo
     email = EmailMessage()
     email["From"] = remitente
     email["To"] = destinatario
-    email["Subject"] = "Codigo de activacion mensajeria Mintic Grupo 13 2022002132"
-    email.set_content(mensaje)
+    email["Subject"] = "Codigo de Activacion"
+    email.set_content(mensaje, subtype="html")
+    smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+    smtp.starttls()
+    smtp.login(remitente, "Martin13")
+    smtp.sendmail(remitente, destinatario, email.as_string())
+    smtp.quit()
+
+
+def recuperar_email(email_destino):
+    remitente = "quinterolf@uninorte.edu.co"
+    destinatario = email_destino
+    mensaje = "<h2>Correo de Restablecimiento de Contraseñas</h2>"
+    mensaje=mensaje+ "<a href='http://localhost:5000/restablecer/"+email_destino+"'>Click para Restablecer la contraseña</a>"
+    mensaje=mensaje+"<hr>"
+    email = EmailMessage()
+    email["From"] = remitente
+    email["To"] = destinatario
+    email["Subject"] = "Recuperar Contraseña"
+    email.set_content(mensaje, subtype="html")
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
     smtp.login(remitente, "Martin13")
